d38c83262d5a7328e42d1e029c0e46c0084952d8
Alan Roussel <alan_roussel@ens.univ-artois.fr>
2023-01-13 11:22:15 +0100

    🎉

---
+++ Source/Steering/SteeringGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteeringGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEERING_API ASteeringGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
